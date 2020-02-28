512d1599663493b65db8bf09b78b4ed49f11271a
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2020-02-28 13:17:53 -0800

    Add files via upload

--- EssentialTraining/ProgrammingLoops.cs
+++ EssentialTraining/ProgrammingLoops.cs
@@ -4,14 +4,50 @@ namespace EssentialTraining
 {
     public class ProgrammingLoops
     {
-        public void ForLoop()
+        public int ForLoop()
         {
             var sum = 0;
             for (var i=0; i<100; i++)
             {
-                sum += i;
+                sum = sum + i;
             }
-            Console.WriteLine(sum);
+            return sum;
+        }
+
+        public int ForEachLoop()
+        {
+            var numbers = new List<int> {1, 3, 5, 7, 9};
+            var sum = 0;
+            foreach(var number in numbers) 
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
+
+        public int WhileLoop()
+        {
+            var sum = 0;
+            var counter = 0;
+            while (counter < 100) {
+                sum += counter;
+                counter += 1;
+            }
+
+            return sum;
+        }
+
+        public int DoWhileLoop()
+        {
+            var sum = 0;
+            var counter = 0;
+            do {
+                sum += counter;
+                counter += 1;
+            } while (counter < 100);
+
+            return sum;
         }
     }
 }
