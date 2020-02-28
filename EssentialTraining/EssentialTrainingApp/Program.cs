00ca9d69e5350bbf0accf473441e120208d1d5d6
Curtis Yungen <curtisyungen@gmail.com>
2020-02-28 15:51:01 -0800

    write in Nlog

--- EssentialTraining/EssentialTrainingApp/Program.cs
+++ EssentialTraining/EssentialTrainingApp/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using NLog;
 
 namespace EssentialTrainingApp
 {
     class Program
     {
+        public static Logger logger = LogManager.GetCurrentClassLogger();
         public static List<string> Words;
+
         static void Main(string[] args)
         {
+            logger.Trace("The program started.");
             Words = new List<string>();
             Words.Add("Bread");
             Words.Add("Milk");
@@ -43,14 +47,17 @@ namespace EssentialTrainingApp
             catch (System.IO.FileNotFoundException ex)
             {
                 Console.WriteLine("Could not find the file.");
+                logger.Error("The file was not found." + ex.Message);
             }
             catch (System.IO.DirectoryNotFoundException ex)
             {
                 Console.WriteLine("Could not find the directory.");
+                logger.Error("The directory was not found." + ex.Message);
             }
             catch (Exception ex)
             {
                 Console.WriteLine("An unknown error occurred " + ex.Message);
+                logger.Error(ex.Message);
             }
             finally
             {
