d0f6da2fb6892b142275500c23ea9b5c2bb842c2
Curtis Yungen <v-cuyung@microsoft.com>
2020-03-10 13:07:45 -0700

    add LL_testDrivenDev

---
+++ LL_TestDrivenDev/SalaryCalculatorTestProject/CalculatorTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Calculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SalaryCalculatorTestProject
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void AnnualSalaryTest()
+        {
+            // Arrange
+            SalaryCalculator sc = new SalaryCalculator();
+
+            // Act
+            decimal annualSalary = sc.GetAnnualSalary(50);
+
+            // Assert
+            Assert.AreEqual(annualSalary, 104000);
+        }
+
+        [TestMethod]
+        public void HourlyWageTest()
+        {
+            // Arrange
+            SalaryCalculator sc = new SalaryCalculator();
+
+            // Act
+            decimal hourlyWage = sc.GetHourlyWage(52000);
+
+            // Assert
+            Assert.AreEqual(hourlyWage, 25);
+        }
+    }
+}
---
+++ LL_TestDrivenDev/SalaryCalculatorTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SalaryCalculatorTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SalaryCalculatorTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c458d156-fa7c-424f-873f-0f6a80c6a608")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
